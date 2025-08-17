4eb618c92beb662f8401ab43aada186ff45dab64
fncoding <felix.coding@proton.me>
2025-08-17 12:35:50 +0000

    alpha-pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
+
 export default defineConfig({
   plugins: [
     react(),
