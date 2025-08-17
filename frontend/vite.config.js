7c641e73078dcf13f388e58f42e04859e442f5e9
fncoding <felix.coding@proton.me>
2025-08-17 13:47:27 +0000

    release PWA

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-
-
 export default defineConfig({
   plugins: [
     react(),
