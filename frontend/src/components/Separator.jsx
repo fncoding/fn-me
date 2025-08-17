e012a9f354c850fa9020542449030e613c8c9256
fncoding <felix.coding@proton.me>
2025-08-17 01:48:06 +0000

    bar fix

--- frontend/src/components/Separator.jsx
+++ frontend/src/components/Separator.jsx
@@ -4,7 +4,7 @@ const Separator = ({ className = '', style = {} }) => (
   <div
     role="separator"
     aria-hidden="true"
-    className={`reveal m-2 w-full h-0.5 my-6 rounded-full bg-gradient-to-r from-[#071033] via-[#064e3b] to-white ${className}`}
+    className={`reveal w-full h-0.5 my-6 rounded-full bg-gradient-to-r from-[#071033] via-[#064e3b] to-white ${className}`}
     style={{ ...style }}
   />
 );
